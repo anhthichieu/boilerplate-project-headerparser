c03bfda6952c3e7b3da6731a62b5b058ab7e0184
anhthichieu <62415015+anhthichieu@users.noreply.github.com>
2021-08-12 16:29:26 +0700

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,14 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/whoami", function(req, res) {
+  console.log('req', req);
+  res.json({
+    ipaddress: req.ip,
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent']
+  });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
